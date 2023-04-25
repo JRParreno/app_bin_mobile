ef143b089e202ee6f5d6eb5fcd684e9cb509d279
JRParreno <jhonrhayparreno22@gmail.com>
2023-04-25 09:15:53 +0800

    added icons and splash image

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,9 @@
-import 'package:app_bin_mobile/src/core/routes/app_route.dart';
-import 'package:app_bin_mobile/src/features/account/login/presentation/screen/login_screen.dart';
+import 'package:app_bin_mobile/src/features/app_bin.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      onGenerateRoute: generateRoute,
-      home: const LoginScreen(),
-    );
-  }
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  runApp(const AppBin());
 }
